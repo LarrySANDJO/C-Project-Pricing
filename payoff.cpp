3ffc434abff09f133d0dea3480c361edf6302c8e
LarrySANDJO <larrysandjo1@gmail.com>
2025-12-29 21:16:00 +0100

    Maybe Last Push

--- payoff.cpp
+++ payoff.cpp
@@ -121,6 +121,9 @@ double LookbackFloatingCallPayoff::operator()(const std::vector<double>& path) c
     return std::max(maxS - S_T, 0.0);
 }
 
+LookbackFloatingCallPayoff::LookbackFloatingCallPayoff()
+    : Payoff(0.0, OptionType::Put) {}
+
 double LookbackFloatingPutPayoff::operator()(const std::vector<double>& path) const
 {
     // Payoff : S_T - min(S_t)
